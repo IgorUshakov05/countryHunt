06e99863f3ddc90ce8c4e1e2438b025bf9484dbc
Igor <igorushakov111@gmail.com>
2024-08-29 07:34:11 +0700

    'Начало'

---
+++ app/components/ux/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return ( 
+        <header>
+            <a href="/"><img src="/logo_of_header.svg" alt="Логотип" /></a>
+        </header>
+     );
+}
+
+export default Header;
